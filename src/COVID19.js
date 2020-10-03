d78c6883397ef46b6d16a334b4e03a291c68721e
Amir Raza <62903819+araza01@users.noreply.github.com>
2020-10-03 22:13:20 +0530

    Add files via upload

---
+++ src/COVID19.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react'
+
+export default class Covid19 extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isLoaded: false,
+            error: null,
+            output: []
+        }
+    }
+    componentDidMount() {
+        fetch("https://api.covid19api.com/world/total")
+        .then(response => response.json())
+        .then((result) => {
+            this.setState({
+                isLoaded: true,
+                output: result
+            });
+        }).catch((error) => {
+            this.setState({
+                isLoaded: true,
+                error
+            });
+        })
+        console.log(this.state.output);
+    }
+
+    render() {
+        const {isLoaded, error, output} = this.state;
+        
+        if(error) {
+            return (
+                <section className="container">
+                    <article className="align">
+                        Error: {error.message}
+                    </article>
+                </section>
+            );
+        } else if(!isLoaded) {
+            return (
+                <section className="container">
+                    <article className="align">
+                        Loading<i className="fa fa-spinner fa-spin"></i>
+                    </article>
+                </section>
+            );
+        }
+        return (
+            <section className="container">
+                <article className="align">
+                    <div className="header">COVID-19 CORONAVIRUS PANDEMIC</div>
+                </article>
+                <article>
+                    <div>Confirmed</div>
+                    <div>
+                        <ul>
+                            {
+                                Object.keys(output).map((stats, i) => {
+                                    return <li key={stats}>{stats.TotalConfirmed}</li>;
+                                })
+                            }
+                        </ul>
+                    </div>
+                </article>
+            </section>
+        );
+    }
+}
