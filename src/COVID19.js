0279b31fa58be6beddee3a22e5bba667bc815cc4
Amir Raza <62903819+araza01@users.noreply.github.com>
2020-10-05 11:01:33 +0530

    Update COVID19.js

--- src/COVID19.js
+++ src/COVID19.js
@@ -8,9 +8,17 @@ export default class Covid19 extends Component {
             error: null,
             output: []
         }
+        this.handleNumbers = this.handleNumbers.bind(this);
     }
+
+    handleNumbers = (num) => {
+        let parts = num.toString().split(".");
+        parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+        return parts.join(".");
+    }
+
     componentDidMount() {
-        fetch("https://api.covid19api.com/world/total")
+        fetch("https://api.covid19api.com/world?from=2020-10-03T23:59:00Z&to=2020-10-04T00:00:00")
         .then(response => response.json())
         .then((result) => {
             this.setState({
@@ -23,7 +31,6 @@ export default class Covid19 extends Component {
                 error
             });
         })
-        console.log(this.state.output);
     }
 
     render() {
@@ -47,23 +54,68 @@ export default class Covid19 extends Component {
             );
         }
         return (
-            <section className="container">
-                <article className="align">
-                    <div className="header">COVID-19 CORONAVIRUS PANDEMIC</div>
-                </article>
-                <article>
-                    <div>Confirmed</div>
-                    <div>
-                        <ul>
+            <React.Fragment>
+                <section className="container">
+                    <article className="align">
+                        <div className="header">COVID-19 CORONAVIRUS PANDEMIC</div>
+                        <div className="sub-header">Coronavirus Cases:</div>
+                    </article>
+                </section>
+
+                <section className="container">
+                    <article className="float-sm-left conf-cases conf-col">
+                        <div className="cases">Total<br />Confirmed</div>
+                        <div className="numbers">
                             {
-                                Object.keys(output).map((stats) => {
-                                    return stats.TotalConfirmed;
+                                output.map((stats) => {
+                                    return this.handleNumbers(stats.TotalConfirmed);
                                 })
                             }
+                        </div>
+                    </article>
+                    
+                    <article className="float-sm-left recov-cases recov-col">
+                        <div className="cases">Total<br />Recovered</div>
+                        <div className="numbers">
+                            {
+                                output.map((stats) => {
+                                    return this.handleNumbers(stats.TotalRecovered);
+                                })
+                            }
+                        </div>
+                    </article>
+                    
+                    <article className="float-sm-left death-cases death-col">
+                        <div className="cases">Total<br />Deaths</div>
+                        <div className="numbers">
+                            {
+                                output.map((stats) => {
+                                    return this.handleNumbers(stats.TotalDeaths);
+                                })
+                            }
+                        </div>
+                    </article>
+                </section>
+
+                <section className="container">
+                    <article className="search-bar">
+                        <input type="text" placeholder="Search by Country" />
+                        <button type="submit">Submit</button>
+                    </article>
+                </section>
+
+                <section className="container">
+                    <article className="info-list">
+                        <ul>
+                            <li>Country</li>
+                            <li>Confirmed</li>
+                            <li>Active</li>
+                            <li>Recovered</li>
+                            <li>Deceased</li>
                         </ul>
-                    </div>
-                </article>
-            </section>
+                    </article>
+                </section>
+            </React.Fragment>
         );
     }
 }
